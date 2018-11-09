cb54454e58a4f4d725a3ffd6ed6d9691f4d0f07d
Gen <ymdtshm@gmail.com>
2018-11-09 21:17:45 +0900

    save

--- gen_blog.rb
+++ gen_blog.rb
@@ -1,5 +1,6 @@
 require 'sinatra/reloader'
 require 'sinatra/base'
+require 'slim'
 require 'erb'
 require 'logger'
 require "date"
@@ -8,29 +9,28 @@ $: << File.join(__dir__, "helper")
 require "application_helper"
 
 class GenApp < Sinatra::Base
-    register Sinatra::Reloader
+  register Sinatra::Reloader
 
-    $logger = Logger.new("#{File.dirname(__FILE__)}/log/logger.log")
-    
-    get '/' do
-        @md_page = "Arch_install"
-        @txt = File.open("#{File.dirname(__FILE__)}/md/#{@md_page}.md", "rb").read
-        @txt.force_encoding("utf-8")
-        @md = ApplicationHelper.markdown(@txt)
-        @title = "Ruby配列あれこれ"
-        @data = Date.today
-        erb :index
-    end
-    
-    get '/create' do
-        erb :create
-    end
+  $logger = Logger.new("#{File.dirname(__FILE__)}/log/logger.log")
 
-    get '/highlight.css' do
-        #Rouge css
-        headers 'Content-Type' => 'text/css'
-        Rouge::Themes::Github.render(scope: '.highlight')
-    end
+  get '/' do
+    @txt = IO.read("#{__dir__}/md/Arch_install.md")
+    @txt.force_encoding("utf-8")
+    @md = ApplicationHelper.markdown(@txt)
+    @title = "Ruby配列あれこれ"
+    @data = Date.today
+    slim :index
+  end
+
+  get '/create' do
+    erb :create
+  end
+
+  get '/highlight.css' do
+    #Rouge css
+    headers 'Content-Type' => 'text/css'
+    Rouge::Themes::Github.render(scope: '.highlight')
+  end
 end
 
 GenApp.run!
